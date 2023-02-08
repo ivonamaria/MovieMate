82e3f04e2d003e5e765557abc0f10ff7d8d8265c
Iwona Malkinska <ivonkaa@hotmail.com>
2023-02-08 21:45:37 +0000

    js file with working API

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+//API from TMDB website
+
+const API_KEY = 'api_key=4d4b3c800fcb1c117f604df7499ce00e';
+const MAIN_URL = 'https://api.themoviedb.org/3'
+const API_URL = MAIN_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+
