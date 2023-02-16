e6509a9b0519ba3916c93a13cbea14e94ebd70f2
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-02-17 05:04:12 +0530

    removed unused code in JS

--- assets/script.js
+++ assets/script.js
@@ -168,7 +168,7 @@ function movieContainer(movies, title = '') {
       ${movieSegment(movies)}
     </section>
     <div class="content">
-      <p id="content-close">X</p>
+      <div id="content-close" type="search">X</div>
     </div>
   `;
 
@@ -323,64 +323,3 @@ nextQuoteEl.addEventListener("click", function (event) {
 	quote();
 });
 
-// //Trending Movies TMDB API
-
-// const movieURL = "https://api.themoviedb.org/3/trending/all/week?api_key=11d709982d73ca3b61226bf899b78a2b";
-// const movieImages = "https://image.tmdb.org/t/p/w500/";
-// const movieSearch = "https://api.themoviedb.org/3/search/movie?&api_key=11d709982d73ca3b61226bf899b78a2b&query=";
-
-// //Display results in browser
-
-// const main = document.getElementById("content");
-
-// getMovies(movieURL);
-
-// async function getMovies(url) {
-//     const resp = await fetch(url);
-//     const respData = await resp.json();
-
-//     console.log(respData);
-
-//     showMovies(respData.results);
-// }
-
-// function showMovies(movies) {
-//     // clear main
-//     main.innerHTML = "";
-
-//     movies.forEach((movie) => {
-//         const { poster_path, title, vote_average, overview } = movie;
-
-//         const movieEl = document.createElement("div");
-//         movieEl.classList.add("movie");
-
-//         movieEl.innerHTML = `
-//             <img
-//                 src="${movieImages + poster_path}"
-//                 alt="${title}"
-//             />
-//             <div class="movie-info">
-//                 <h3>${title}</h3>
-//                 <span class="${getClassByRate(
-//                     vote_average
-//                 )}">${vote_average}</span>
-//             </div>
-//             <div class="overview">
-//                 <h3>Overview:</h3>
-//                 ${overview}
-//             </div>
-//         `;
-
-//         main.appendChild(movieEl);
-//     });
-// }
-
-// function getClassByRate(vote) {
-//     if (vote >= 8) {
-//         return "green";
-//     } else if (vote >= 5) {
-//         return "orange";
-//     } else {
-//         return "red";
-//     }
-// };
